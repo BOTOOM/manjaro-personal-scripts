67e4292d3a3210284f0c17eab4beffd2ae325011
ediaz <ediaz@teravisiontech.com>
2024-08-21 11:54:11 -0500

    add tool coment

--- developer-tools.sh
+++ developer-tools.sh
@@ -2,3 +2,9 @@
 yay -S zip zoom visual-studio-code-bin docker-compose postman-bin google-chrome localsend-bin
 pip install aws-sam-cli 
 sam --version
+
+# expanso todoist supabase brew cisco conect 
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+#  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/botom/.bashrc  
+#  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >>  ~/.zshrc  
